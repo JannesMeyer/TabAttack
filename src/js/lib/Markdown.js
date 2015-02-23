c2a2237f3d2ace6ab1e74f15b88b49b3bcebcd19
Jannes <jannes.meyer@gmail.com>
2015-02-23 11:27:07 +0100

    Never produce empty Markdown links

--- src/js/lib/Markdown.js
+++ src/js/lib/Markdown.js
@@ -2,6 +2,9 @@
  * Create markdown link with proper escaping
  */
 export function markdownLink(title, url) {
+	if (title === '') {
+		title = '(untitled link)';
+	}
 	// Escape special characters
 	title = title.replace(/[\[\]\*\`_\\]/g, c => '\\' + c).replace(/</g, '&lt;');
 	url = url.replace(/[\(\)]/g, escape);
