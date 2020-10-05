0a2c62e1bf3d4d7d83898388f93cdd51adb9fd30
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-06 01:20:28 +0200

    Add icon test

---
+++ src/icon-test.tsx
@@ -0,0 +1,22 @@
+// import drawIcon, { createCanvas, getRenderingContext, loadFonts } from "./tabcount/draw";
+
+// var button = document.querySelector('button');
+// if (button == null) {
+//   throw new Error();
+// }
+// button.addEventListener('click', applyTheme);
+
+
+
+// loadFonts().then(() => {
+//   let scale = devicePixelRatio;
+//   for (let i = 1; i <= 500; ++i) {
+//     let canvas = createCanvas(16 * scale);
+//     let ctx = getRenderingContext(canvas, true);
+//     drawIcon(ctx, i.toString(), scale, '#000');
+//     document.body.appendChild(canvas);
+//     if (i % 10 === 0) {
+//       document.body.appendChild(document.createElement('br'));
+//     }
+//   }
+// });
