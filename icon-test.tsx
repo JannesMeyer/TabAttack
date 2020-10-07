8465ff76c9cb41ae80aa62c0534347d33f7b5420
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-07 16:36:29 +0200

    Add dark mode to the icon test page

--- icon-test.tsx
+++ icon-test.tsx
@@ -2,10 +2,24 @@ import Icon from './background/Icon.js';
 import loadFont from './fonts/loadFont.js';
 
 const scales = [1, 1.25, 1.5, 1.75, 2, 2.5, 3, 4];
+const prefersDark = matchMedia('(prefers-color-scheme: dark)');
+const icons = Array(500).fill(null).map((_, i) => new Icon(scales[i % scales.length]));
 
+// Wait for fonts to load
 Promise.all([
 	loadFont('Roboto', '/fonts/Roboto-Bold.woff2'),
 	loadFont('Roboto Condensed', '/fonts/Roboto-Condensed-Bold.woff2'),
-]).then(() => Array(500).fill(null).forEach((_, i) => {
-	document.body.appendChild(new Icon(scales[i % scales.length], '#000').render(++i).canvas);
-}));
+]).then(() => {
+	icons.forEach(({ canvas }) => document.body.appendChild(canvas));
+	renderIcons();
+	prefersDark.addEventListener('change', renderIcons);
+});
+
+/** Renders all icons */
+function renderIcons() {
+	let color = (prefersDark.matches ? '#fff' : '#000');
+	icons.forEach((icon, i) => {
+		icon.textColor = color;
+		icon.render(++i);
+	});
+}
