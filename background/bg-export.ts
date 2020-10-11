050672f47253abf7439ef4f10f0dbba0871edf18
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-11 15:57:52 +0200

    Improve bg-export

--- background/bg-export.ts
+++ background/bg-export.ts
@@ -2,24 +2,21 @@ import onCommand from '../lib/browser/onCommand.js';
 import ContextMenuItem from '../lib/ContextMenuItem.js';
 import logError from '../lib/logError.js';
 import getActiveTab from '../lib/browser/getActiveTab.js';
-import assertDefined from '../lib/assertDefined.js';
+import UrlQuery from '../lib/dom/UrlQuery.js';
 
 // All windows
-browser.browserAction.onClicked.addListener(tab => openTabsEditor(tab).catch(logError));
+browser.browserAction.onClicked.addListener(tab => openTabsEditor(tab));
 
 // Only current window
 new ContextMenuItem({
 	id: 'export_current_window',
 	contexts: ['browser_action'],
-	onclick: (_, tab) => openTabsEditor(tab, true).catch(logError),
+	onclick: (_, tab) => openTabsEditor(tab, true),
 });
-onCommand('export_current_window', () => getActiveTab().then(tab => openTabsEditor(tab, true)).catch(logError));
+onCommand('export_current_window', () => getActiveTab().then(tab => openTabsEditor(tab, true)));
 
 /** Open the tab editor in a new tab */
-function openTabsEditor(sourceTab: browser.tabs.Tab, onlyCurrentWindow = false) {
-	let url = browser.runtime.getURL('tabs.html');
-	if (onlyCurrentWindow) {
-		url += '?w=' + assertDefined(sourceTab.windowId);
-	}
-	return browser.tabs.create({ url, openerTabId: sourceTab.id });
+function openTabsEditor({ id, windowId }: browser.tabs.Tab, single?: true) {
+	let url = browser.runtime.getURL('tabs.html') + new UrlQuery({ t: id, w: windowId, single });
+	return browser.tabs.create({ url, openerTabId: id }).catch(logError);
 }
