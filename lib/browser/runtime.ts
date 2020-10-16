7a9bb21c9461fb4eb2ada2c284f6e163627e41dd
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-16 11:43:59 +0200

    Remove chromelike

--- lib/browser/runtime.ts
+++ lib/browser/runtime.ts
@@ -1,3 +1,2 @@
 export const baseUrl = browser.runtime.getURL('');
 export const isFirefox = baseUrl.startsWith('moz-extension://');
-export const isChromeLike = baseUrl.startsWith('chrome-extension://');
