eaa731096965e5a96932cdc2889829d651be4f13
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-19 12:35:13 +0200

    Fix build script

--- build.ps1
+++ build.ps1
@@ -1,20 +1,33 @@
-$compress = @{
-  Path =
-		"_locales",
-		"dist",
-		"fonts",
-		"icons",
-		"images",
-		"*.css",
-		"*.html",
-		"manifest.json",
-		"LICENSE",
-		"node_modules/webextension-polyfill/dist/browser-polyfill.js",
-		"node_modules/react/umd/react.development.js",
-		"node_modules/react-dom/umd/react-dom.development.js",
-		"node_modules/ace-builds/src-noconflict",
-		"node_modules/marked/lib/marked.js"
-  DestinationPath = "TabAttack.zip"
-  Force = $true
-}
-Compress-Archive @compress
+$t = mkdir build
+
+./node_modules/.bin/tsc --sourceMap false --outDir $t/dist
+Copy-Item -Recurse _locales, icons, fonts, images $t
+Copy-Item *.css, *.html, manifest.json, LICENSE $t
+
+# marked
+$m = mkdir $t/node_modules/marked/lib
+Copy-Item node_modules/marked/marked.min.js $m/marked.js
+Copy-Item node_modules/marked/LICENSE.md $m/../LICENSE.md
+
+# webextension-polyfill
+$m = mkdir $t/node_modules/webextension-polyfill/dist
+Copy-Item node_modules/webextension-polyfill/dist/browser-polyfill.min.js $m/browser-polyfill.js
+Copy-Item node_modules/webextension-polyfill/LICENSE $m/../LICENSE
+
+# react
+$m = mkdir $t/node_modules/react/umd
+Copy-Item node_modules/react/umd/react.production.min.js $m/react.development.js
+Copy-Item node_modules/react/LICENSE $m/../LICENSE
+
+# react-dom
+$m = mkdir $t/node_modules/react-dom/umd
+Copy-Item node_modules/react-dom/umd/react-dom.production.min.js $m/react-dom.development.js
+Copy-Item node_modules/react-dom/LICENSE $m/../LICENSE
+
+# ace-builds
+$m = mkdir $t/node_modules/ace-builds
+Copy-Item -Recurse node_modules/ace-builds/src-min-noconflict $m/src-noconflict
+Copy-Item node_modules/ace-builds/LICENSE $m/LICENSE
+
+Compress-Archive $t/* TabAttack.zip -Force
+Remove-Item -Recurse $t
