e0dbc067897295259ed86132e050b0124b689db1
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-21 10:35:19 +0200

    Add "this" support

--- lib/debounce.ts
+++ lib/debounce.ts
@@ -1,7 +1,7 @@
-export default function debounce<F extends (...args: any[]) => void>(fn: F, wait: number) {
+export default function debounce<F extends (...args: any[]) => void>(fn: F, delay: number) {
 	let id: number | undefined;
-	return function(...args: Parameters<F>) {
+	return function(this: ThisParameterType<F>, ...args: Parameters<F>) {
 		clearTimeout(id);
-		id = setTimeout(() => fn(...args), wait);
+		id = setTimeout(() => fn.apply(this, args), delay);
 	};
 }
