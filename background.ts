5220fd49c14b0e0facf0d05e5f294f6d35b9fb15
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-21 12:56:58 +0200

    Remove old file

--- background.ts
+++ background.ts
@@ -1,6 +1,5 @@
 import './lib/Array.extensions.js';
 import './background/bg-icon.js';
-import './background/bg-export.js';
 import './background/bg-copy-link.js';
 import './background/bg-keyboard.js';
 import './background/bg-popup.js';
