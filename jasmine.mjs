11a44a58063a743a1cabc3c118ffab0854d60e1a
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-29 13:46:17 +0100

    Fail on weird exports

--- jasmine.mjs
+++ jasmine.mjs
@@ -10,9 +10,6 @@ glob('./dist/**/*' + ext, async (err, files) => {
 	let specials = { beforeEach, afterEach, beforeAll, afterAll };
 	await Promise.all(files.map(f => import(f).then(suite => describe(path.basename(f, ext), () => {
 		for (let [n, fn] of Object.entries(suite)) {
-			if (typeof fn !== 'function') {
-				continue;
-			}
 			if (specials.hasOwnProperty(n)) {
 				specials[n](fn);
 			} else if (fn.x) {
