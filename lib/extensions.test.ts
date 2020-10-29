7d36ac9418feeec2759a18951da1b707217cb7ac
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-29 14:07:47 +0100

    Add test

--- lib/extensions.test.ts
+++ lib/extensions.test.ts
@@ -9,3 +9,9 @@ export function arraySingle() {
 	expect([1].single()).toBe(1);
 	expect(() => [1, 2].single()).toThrowError();
 }
+
+export function mapGet() {
+	let m = new Map<number, number>();
+	expect(() => m.getOrThrow(undefined)).toThrowError();
+	expect(m.get(undefined)).toBeUndefined();
+}
