1f70f2b213241de702029b1d0948291f22ba2f3d
Jannes Meyer <jannes.meyer@gmail.com>
2020-10-31 20:49:02 +0100

    Improve watcher

--- jasmine-watch.mjs
+++ jasmine-watch.mjs
@@ -2,9 +2,15 @@ import fs from 'fs';
 import child_process from 'child_process';
 import debounce from './dist/lib/debounce.js';
 
+const dir = 'dist';
+const debounceMs = 200;
+const scriptPath = 'jasmine.mjs';
+
 run();
-let handleChange = debounce(run, 200);
-fs.watch('./dist', { recursive: true }, ev => ev === 'change' && handleChange());
+fs.watch(dir, { recursive: true })
+	.on('change', debounce(run, debounceMs))
+	.on('error', console.error);
+
 function run() {
-	child_process.fork('jasmine.mjs').addListener('error', console.error);
+	child_process.fork(scriptPath).on('error', console.error);
 }
