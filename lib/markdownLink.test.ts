eb69c09f2ebf9835cc3578567031fc2c86ffd175
Jannes Meyer <jannes.meyer@gmail.com>
2020-11-06 10:07:02 +0100

    Update markdownLink.test.ts

--- lib/markdownLink.test.ts
+++ lib/markdownLink.test.ts
@@ -1,6 +1,6 @@
 import markdownLink from './markdownLink.js';
 
 export function escapesTitle() {
-	expect(markdownLink('', 'https://example.com/?asd=\')')).toBe('[Untitled](https://example.com/?asd=\'%29)');
+	expect(markdownLink('', 'https://example.com/?a=\')')).toBe('[Untitled](https://example.com/?a=\'%29)');
 	expect(markdownLink('[]*`_\\<>', 'https://example.com/')).toBe('[\\[\\]\\*\\`\\_\\\\&lt;>](https://example.com/)');
 }
